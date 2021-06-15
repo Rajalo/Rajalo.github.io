bc478f7d5547268610defc0345d5bf41fa03c00c
Reilly <40543087+Rajalo@users.noreply.github.com>
2021-06-15 19:49:12 -0400

    Added Dream Run content
    added content related to Dream Run

--- src/PixelRender.js
+++ src/PixelRender.js
@@ -1,9 +1,10 @@
 'use strict'
-var folders = ["Astronaut Ascending","BunjeeSkate", "Rose Meadow Farms", "TSP Calculator"];
+var folders = ["Astronaut Ascending","BunjeeSkate", "Rose Meadow Farms", "TSP Calculator", "Dream Run"];
 var fields = [["Red Astronaut Riding Equmander.gif" ,"Red Astronaut.gif" ,"Spaceship Passive.gif", "Ziward.gif" ],
 ["Bouncing Player.gif","Moose Running.gif","Moose Still.gif","Shooting  Player.gif","Skating Player.gif","Stuck Player.gif","Test Run.gif" ], 
 ["FarmerPlant1.gif","FarmerShoo.gif","PigRun.gif","RatRun.gif","RoseUp.gif","SnakeRun.gif","tutorial1.gif" ],
-["Add Hover.png","Add Pressed.png","Add.png","Clear Hover.png","Clear Pressed.png","Clear.png","Point Excluded and Selected.gif","Point Included and Selected.gif","Tour Hover.png","Tour Pressed.png","Tour.png" ]
+["Add Hover.png","Add Pressed.png","Add.png","Clear Hover.png","Clear Pressed.png","Clear.png","Point Excluded and Selected.gif","Point Included and Selected.gif","Tour Hover.png","Tour Pressed.png","Tour.png" ],
+["Go button on.gif", "green elephant.gif", "ice cream.gif", "Player Jump.gif" ]
 ];
 
 function generateInnerHtml()
